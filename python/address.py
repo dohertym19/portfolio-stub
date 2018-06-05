54d76274de3a1a041c4c85df052ed8fde6cfa868
User <user@vagelos-ve735-0637.apnd.wlan.private.upenn.edu>
2018-06-05 14:54:18 -0400

    all the works

---
+++ python/address.py
@@ -0,0 +1,7 @@
+address_exercise = {
+"name" : "Megan Doherty",
+"street" : "666 Python St.",
+"state_zip": "Lexington, VA 24450"}
+print (address_exercise["name"])
+print (address_exercise["street"])
+print (address_exercise["state_zip"])
---
+++ python/seconds_years.py
@@ -0,0 +1,3 @@
+x = 600000000
+computation = x/31557600
+print ("The number of years in " + str(x) + " seconds is " + str(computation) + " years.")
