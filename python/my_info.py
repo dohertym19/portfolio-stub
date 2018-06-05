1f5eb9ff52056223d113d86f844525cd5f29b5f0
User <user@vagelos-ve735-0637.apnd.wlan.private.upenn.edu>
2018-06-05 11:22:21 -0400

    more things with python

--- python/my_info.py
+++ python/my_info.py
@@ -1,1 +1,5 @@
 my_info = {"first_name": "Megan", "last_name": "Doherty", "hometown": "Morristown, TN", "favorite_food": "potato"}
+my_info["first_name"]
+my_info["last_name"]
+my_info["hometown"]
+my_info["favorite_food"]
