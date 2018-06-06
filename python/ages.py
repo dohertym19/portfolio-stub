738aa13b18ef2154b00ff95ce9fa24f3bdfb9855
User <user@vagelos-ve735-0637.apnd.wlan.private.upenn.edu>
2018-06-06 10:17:38 -0400

    definitely not finished may have fucked up

---
+++ python/ages.py
@@ -0,0 +1,6 @@
+if age > 0 and age<=2:
+     print("baby")
+elif age >2 and age <18:
+     print("child")
+else:
+     print(adult)
---
+++ python/authors.py
@@ -0,0 +1,8 @@
+authors = {
+"Charles Dickens, 1870"
+"William Thackeray, 1863"
+"Anthony Trollope, 1882"
+"Gerard Manley Hopkins, 1889"
+}
+def add (author):
+    print(name, date in
